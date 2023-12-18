0bf3241c985e9e27070a0597e6ca61c769736ecb
Jimmy Karoly <64671049+PAJK87@users.noreply.github.com>
2023-12-17 19:16:16 -0500

    Add files via upload

---
+++ Q5.sql
@@ -0,0 +1,5 @@
+SELECT name, routes, color FROM nyc_subway_stations ORDER BY routes ASC;
+
+SELECT name, routes, color FROM nyc_subway_stations ORDER BY routes ASC LIMIT 10;
+
+SELECT name, routes, color FROM nyc_subway_stations ORDER BY name DESC;
